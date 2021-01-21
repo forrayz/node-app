3b3c8c8e030a315677032f42515130c95f8aa6ad
forrayz <forray.zoltan@ulyssys.com>
2021-01-21 16:28:21 +0100

    n8n

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # node_app
 
+#### for local testing 
+add this to /etc/localhost
+127.0.0.1 n8n.development.localhost.local adminer.development.localhost.local log.development.localhost.local 
+
+
+```
+
+```
+
+
+
 ## 5 misc commands
 ##### 5.1 erase all stuff (bash/fish)
 ```bash
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,29 +8,28 @@ echo "##############################################"
 sed -n '/MainConfigBegins/,/MainConfigEnds/p' .env > bootstrap.env
 source bootstrap.env
 
-ENVIRONMENT_PREFIX=$1
-echo "argument-ENVIRONMENT_PREFIX---->$ENVIRONMENT_PREFIX"
-echo "argument-MONGO_USERNAME-------->$MONGO_USERNAME"
-echo "argument-MONGO_USERNAME-------->$MONGO_PASSWORD"
-echo "argument-MONGO_PORT------------>$MONGO_PORT"
-echo "argument-MONGO_DB-------------->$MONGO_DB"
+export ENVIRONMENT_PREFIX=$1
+export DOMAIN=$2
 
-APP_CONTAINER_NAME=$ENVIRONMENT_PREFIX"_app_1"
-DB_CONTAINER_NAME=$ENVIRONMENT_PREFIX"_db_1"
+export POSTGRES_USER=$POSTGRES_USER
+export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+export POSTGRES_DB=$POSTGRES_DB
 
 
-docker-compose --compatibility -p $ENVIRONMENT_PREFIX up --force-recreate --no-deps --detach 
 
-docker exec ${ENVIRONMENT_PREFIX}_app_1 sh -c "node --version"
+
+docker-compose --compatibility -p ${ENVIRONMENT_PREFIX} up --build --force-recreate --no-deps --detach
+docker exec ${ENVIRONMENT_PREFIX}_n8n_1 sh -c "node --version"
 docker network connect ${ENVIRONMENT_PREFIX}_default nginx || true
 
-
-DATABASE_PORT=$(docker port ${ENVIRONMENT_PREFIX}_db_1 | cut -d ':' -f 2)
+DATABASE_PORT=$(docker port ${ENVIRONMENT_PREFIX}_postgres_1 | cut -d ':' -f 2)
 ( echo "cat <<EOF" ; cat services.html ; echo EOF ) | sh > ${ENVIRONMENT_PREFIX}.html     
-docker cp ${ENVIRONMENT_PREFIX}.html service-discovery:/web
+docker cp ${ENVIRONMENT_PREFIX}.html service-discovery:/web/
 ( echo "cat <<EOF" ; cat nginx-template.conf ; echo EOF ) | sh > ${ENVIRONMENT_PREFIX}.conf
 docker cp ${ENVIRONMENT_PREFIX}.conf nginx:/etc/nginx/conf.d
 docker exec nginx sh -c "nginx -t && nginx -s reload"
-docker logs ${ENVIRONMENT_PREFIX}_app_1
+docker logs ${ENVIRONMENT_PREFIX}_n8n_1
 
 docker-compose -p $ENVIRONMENT_PREFIX restart
+docker-compose -p $ENVIRONMENT_PREFIX ps
+
